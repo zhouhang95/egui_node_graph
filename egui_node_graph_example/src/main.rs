881fd3869765c6156078daf3dcd5e55ddecf02e0
zhouhang95 <765229842@qq.com>
2024-02-08 11:55:27 +0800

    chinese font

--- egui_node_graph_example/src/main.rs
+++ egui_node_graph_example/src/main.rs
@@ -3,7 +3,39 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use eframe::egui;
 use mme_shader_graph::NodeGraphExample;
+
+fn setup_custom_fonts(ctx: &egui::Context) {
+    // Start with the default fonts (we will be adding to them rather than replacing them).
+    let mut fonts = egui::FontDefinitions::default();
+
+    // Install my own font (maybe supporting non-latin characters).
+    // .ttf and .otf files supported.
+    fonts.font_data.insert(
+        "my_font".to_owned(),
+        egui::FontData::from_static(include_bytes!(
+            "../../assets/HarmonyOS_Sans_SC_Light.ttf"
+        )),
+    );
+
+    // Put my font first (highest priority) for proportional text:
+    fonts
+        .families
+        .entry(egui::FontFamily::Proportional)
+        .or_default()
+        .insert(0, "my_font".to_owned());
+
+    // Put my font as last fallback for monospace:
+    fonts
+        .families
+        .entry(egui::FontFamily::Monospace)
+        .or_default()
+        .push("my_font".to_owned());
+
+    // Tell egui to use these fonts:
+    ctx.set_fonts(fonts);
+}
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
@@ -14,6 +46,7 @@ fn main() {
         "MME Shader Graph",
         eframe::NativeOptions::default(),
         Box::new(|cc| {
+            setup_custom_fonts(&cc.egui_ctx);
             cc.egui_ctx.set_visuals(Visuals::dark());
             Box::<NodeGraphExample>::default()
         }),
